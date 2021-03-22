ad7926af85325c737a07ab4471931c55d4a8d856
matte <matte@DESKTOP-C6A18PK.lan>
2021-03-23 00:37:33 +0100

    Interfaccia grafica e import da scene builder su eclipse

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -6,20 +6,57 @@ package it.polito.tdp.spellchecker;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.spellchecker.model.Model;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class FXMLController {
+	
+	private Model model;
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private Label txtNumberOfErrors;
+
+    @FXML
+    private Label txtOperationTime;
+
+    @FXML
+    void handleClearText(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleEnglish(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleItaliano(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSpellCheck(ActionEvent event) {
+
+    }
+    
+    public void setModel(Model model) {
+    	this.model = model;
+    }	
+
+    @FXML
     void initialize() {
+        assert txtNumberOfErrors != null : "fx:id=\"txtNumberOfErrors\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtOperationTime != null : "fx:id=\"txtOperationTime\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
-
 
